2e09b0a4923afaa9ccb50b3e98791c2cc33edd36
codelocker9 <131526840+codelocker9@users.noreply.github.com>
2024-09-05 19:25:32 +0000

    second_last

--- static/main.js
+++ static/main.js
@@ -1,3 +1,8 @@
 const d = new Date();
 let year = d.getFullYear();
 document.getElementById("year").innerHTML = year;
+// minutes
+
+const m = new Date();
+let minutes = m.getMinutes();
+document.getElementById("min").innerHTML = minutes;
